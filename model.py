fe11b0c5fb751f93c046845530d61b79336ddc81
Tijmen van Etten <tijmenvanetten@wcw-stud-145-109-30-215.wireless.uva.nl>
2022-01-11 18:43:45 +0100

    Almost finished model

--- model.py
+++ model.py
@@ -17,27 +17,13 @@
 Main file for Question 1.2 of the assignment. You are allowed to add additional
 imports if you want.
 """
-from collections import defaultdict
-import os
-import json
-import argparse
-import numpy as np
-from torch.autograd.grad_mode import inference_mode
-from tqdm import tqdm
-import matplotlib.pyplot as plt
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torch.utils.data as data
 
-import torchvision
 import torchvision.models as models
-from torchvision import transforms
 
-from cifar10_utils import get_train_validation_set, get_test_set
-
-LAMBDA = 0.7
+import numpy as np
 
 def set_seed(seed):
     """
@@ -52,52 +38,79 @@ def set_seed(seed):
     torch.backends.cudnn.benchmark = False
 
 
-def get_model(model_name, pretrained):
+def get_model(dataset_name, pretrained=True):
     """
-    Returns the model architecture for the provided model_name. 
-
-    Args:
-        model_name: Name of the model architecture to be returned. 
-                    Options: ['debug', 'vgg11', 'vgg11_bn', 'resnet18', 
-                              'resnet34', 'densenet121']
-                    All models except debug are taking from the torchvision library.
-        num_classes: Number of classes for the final layer (for CIFAR10 by default 10)
-    Returns:
-        cnn_model: nn.Module object representing the model architecture.
+    Returns the model architecture for the provided dataset_name. 
     """
-    if model_name == 'debug':  # Use this model for debugging
+    # TODO:
+    ADULT_DATASET_FEATURE_SIZE = 80
+    if dataset_name == 'adult':
         model = nn.Sequential(
-            nn.Flatten(),
-            nn.Linear(32*32*3, 80)
-        )
-    elif model_name == 'resnet50':
+            nn.Linear(ADULT_DATASET_FEATURE_SIZE, 80),
+            nn.SELU()
+            )
+
+    elif dataset_name == 'celeba':
         model = models.resnet50(pretrained=pretrained)
         model = torch.nn.Sequential(*(list(model.children())[:-1]))
-    elif model_name =='BERT':
-        model = ...
+
+    elif dataset_name == 'civilcomments':
+        bert_model = torch.hub.load('huggingface/pytorch-transformers', 'model', 'bert-base-uncased')
+        print(bert_model)
+        fc_model = nn.Sequential(
+            nn.Linear(1024, 80),
+            nn.SELU()
+            )
+        model = torch.nn.Sequential(bert_model, fc_model)
+
+    elif dataset_name =='chexpert':
+        model = models.densenet121(pretrained=pretrained)
+        model = torch.nn.Sequential(*(list(model.children())[:-1]))
     else:
-        assert False, f'Unknown network architecture \"{model_name}\"'
+        assert False, f'Unknown network architecture \"{dataset_name}\"'
         
     return model
+
+def split(x, d):
+    sorter = torch.argsort(d)
+    _, counts = torch.unique(d, return_counts=True)
+    return sorter, torch.split(x[sorter], counts.tolist())
 
 
 class FairClassifier(nn.Module):
     def __init__(self, input_model):
         super(FairClassifier, self).__init__()
-        self.input_model = get_model(input_model)
-        in_features = ...
+        self.featurizer = get_model(input_model)
 
-        self.featurizer = nn.Sequential(
-            nn.Linear(in_features, 80),
-            nn.SELU(),
-            nn.Linear(80, 1))
-        self.cla
+        print(self.featurizer.modules)
+        feature_size = self.featurizer.modules[-1].out_features
 
-    def forward(self, x):
-        x = self.input_model(x)
-        x = self.featurizer(x)
+        # Fully Connected models for binary classes
+        self.fc0 = nn.Linear(feature_size, 1)
+        self.fc1 = nn.Linear(feature_size, 1)
 
+        # Join Classifier T
+        self.joint_classifier = nn.Linear(feature_size, 1)
 
-        joint_classification = self.classifier(x)
+    def forward(self, x, d_true, d_random):
+        features = self.featurizer(x)
 
-        return joint_classification, group_specific_loss, group_agnostic_loss
+        joint_y = self.joint_classifier(features)
+
+        # Split on random sampled d-values
+        random_indices, random_d_0, random_d_1 = split(features, d_random)
+
+        # Split on true d-values
+        group_indices, group_d_0, group_d_1 = split(features, d_true)
+
+        group_agnostic_y = torch.zeros(features.shape)
+        group_specific_y = torch.zeros(features.shape)
+
+        group_agnostic_y[random_indices] = torch.cat([self.fc0(random_d_0), self.fc1(random_d_1)])
+        group_specific_y[group_indices] = torch.cat([self.fc0(group_d_0), self.fc1(group_d_1)])
+
+        return F.sigmoid(joint_y), F.sigmoid(group_specific_y), F.sigmoid(group_agnostic_y)
+
+if __name__ == "__main__":
+    model = FairClassifier('adult')
+    print(model)
