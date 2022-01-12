81271c5c5c222ac630fba5671ca9dbc6da15ebd9
Fabian van Stijn <fabianvanstijn@gmail.com>
2022-01-12 19:20:15 +0100

    Civil Comments dataset download manual en ssh toegevoegd.

--- get_data.sh
+++ get_data.sh
@@ -22,3 +22,12 @@ URL="" # See README.md
 cd chexpert
 [[ ! -f "CheXpert-v1.0-small.zip" ]] && wget $URL
 cd ..
+
+# CIVIL COMMENTS
+# Before starting you need to install kaggle with pip or anaconda
+# First you need to register and verify on Kaggle and accept the competition rules.
+# Then you need to download the API token and put it in the .kaggle folder of your user account.
+# This file is called kaggle.json 
+[[ ! -d "Civil" ]] &&  mkdir civil
+cd civil
+kaggle competitions download -c jigsaw-unintended-bias-in-toxicity-classification
