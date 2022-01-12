800875cd622007a20d9f552ef03f93fa0808b21c
lodewijkvkeizerswaard <lodewijk.keizer@gmail.com>
2022-01-12 14:59:49 +0100

    Preprocessing for Adult added

--- data.py
+++ data.py
@@ -1,4 +1,5 @@
 import os
+import torch
 import pandas as pd
 import torch.utils.data as data
 from torchvision.datasets import CelebA
@@ -8,13 +9,29 @@ class AdultDataset(data.Dataset):
         datapath = root + "/adult"
         assert os.path.exists(datapath), "Adult dataset not found! Did you run `get_data.sh`?"
 
-        self._filename = "adult.test" if split == "test" else "adult.test"
-        self._data = pd.read_csv(datapath + "/" + self._filename)
+        self._filename = "adult.test" if split == "test" else "adult.data"
+        data = pd.read_csv(datapath + "/" + self._filename, \
+            names=['age', 'workclass', 'fnlwgt', 'education', 'education-num', 'marital-status',\
+                   'occupation', 'relationship', 'race', 'sex', 'capital-gain', 'capital-loss',\
+                   'hours-per-week', 'native-country', 'salary'])[1:]
+
+        # One-hot encode categorical data
+        for colum in ['workclass', 'education', 'marital-status', 'occupation', 'relationship', 'race', 'sex', 'native-country', 'salary']:
+            data = data[data[colum] != ' ?']
+            onehot_colum = pd.get_dummies(data[colum], prefix=colum)
+            data = pd.merge(left=data, right=onehot_colum, left_index=True, right_index=True)
+            data = data.drop(columns=colum)
+        
+        self._data = data
 
     def __getitem__(self, i):
-        # For now the preprocessing steps are unknown, so we just return the table row
-        item = self._data.values[i]
-        return item
+        # Alias the datafram
+        df = self._data
+        df_x = df.loc[:, ~df.columns.isin(['salary_ <=50K', 'salary_ >50K', 'sex_ Female' ,'sex_ Male'])]
+        x = torch.Tensor(df_x.values[i])
+        t = torch.Tensor([df.iloc[i]['salary_ <=50K'], df.iloc[i]['salary_ >50K']])
+        d = torch.Tensor([df.iloc[i]['sex_ Female'], df.iloc[i]['sex_ Male']])
+        return x, t, d
 
 
 def get_adult(root="data"):
@@ -39,4 +56,7 @@ def get_chexpert(root="data"):
 
 if __name__ == "__main__":
     dummy = get_adult()
-    dummy2 = get_celeba()
+    a = dummy[0][1]
+    print(dummy[0]._data.head())
+    print(a)
+    # dummy2 = get_celeba()
