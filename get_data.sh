9bfbc4492afffa91add8e1edfcc268cb9310e7c3
Fabian van Stijn <fabianvanstijn@gmail.com>
2022-01-13 21:11:17 +0100

    Pillow bij environment, Celeba bij data.py en get_data.sh

--- get_data.sh
+++ get_data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# This script downloads all datasets used for this project into the `data` 
-# directory. To be able to obtain all data, some preparing steps have to be 
+# This script downloads all datasets used for this project into the `data`
+# directory. To be able to obtain all data, some preparing steps have to be
 # taken. Please see the dataset specific steps in `README.md`.
 
 cd data
@@ -38,13 +38,31 @@ else
     cd ..
 fi
 
-# CIVIL COMMENTS DATASET
-# Before starting you need to install kaggle with pip or anaconda
-# First you need to register and verify on Kaggle and accept the competition rules.
-# Then you need to download the API token and put it in the .kaggle folder of your user account.
-# This file is called kaggle.json
-[[ ! -d "civil" ]] &&  mkdir civil
-cd civil
-kaggle competitions download -c jigsaw-unintended-bias-in-toxicity-classification
-cd ..
+##########################
+# CIVIL COMMENTS DATASET #
+##########################
+# CHECK IF THIS IS CORRECT WITH THE OUTPUT OF THE BERT MODEL!!!!!
+if [[ -f civil/train.csv && -f civil/valid.csv && -d civil/train && civil/valid ]]; then
+    echo "Civil Comments dataset found!"
+else
+    # Before starting you need to install kaggle with pip or anaconda
+    # First you need to register and verify on Kaggle and accept the competition rules.
+    # Then you need to download the API token and put it in the .kaggle folder of your user account.
+    # This file is called kaggle.json
+    [[ ! -d "civil" ]] &&  mkdir civil
+    cd civil
+    kaggle competitions download -c jigsaw-unintended-bias-in-toxicity-classification
+    cd ..
+fi
 
+##################
+# CELEBA DATASET #
+##################
+# CHECK IF THIS IS CORRECT WITH THE OUTPUT OF THE BERT MODEL!!!!!
+if [[ -f celeba/train.csv && -f celeba/valid.csv && -d celeba/train && celeba/valid ]]; then
+    echo "CelebA dataset found!"
+else
+    [[ ! -d "celeba" ]] &&  mkdir celeba
+    cd celeba
+     
+fi
