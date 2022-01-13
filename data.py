374275d10f08f5b7997a71d8c29edbe558ff1f76
lodewijkvkeizerswaard <lodewijk.keizer@gmail.com>
2022-01-13 12:25:32 +0100

    Added Adult continous normalization concept!

--- data.py
+++ data.py
@@ -18,26 +18,34 @@ class AdultDataset(data.Dataset):
         assert os.path.exists(datapath), "Adult dataset not found! Did you run `get_data.sh`?"
 
         self._filename = "adult.test" if split == "test" else "adult.data"
-        data = pd.read_csv(datapath + "/" + self._filename, \
+        table = pd.read_csv(datapath + "/" + self._filename, \
             names=['age', 'workclass', 'fnlwgt', 'education', 'education-num', 'marital-status',\
                    'occupation', 'relationship', 'race', 'sex', 'capital-gain', 'capital-loss',\
                    'hours-per-week', 'native-country', 'salary'])[1:]
 
         # One-hot encode categorical data
         for colum in ['workclass', 'education', 'marital-status', 'occupation', 'relationship', 'race', 'sex', 'native-country', 'salary']:
-            data = data[data[colum] != ' ?']
-            onehot_colum = pd.get_dummies(data[colum], prefix=colum)
-            data = pd.merge(left=data, right=onehot_colum, left_index=True, right_index=True)
-            data = data.drop(columns=colum)
+            table = table[table[colum] != ' ?']
+            onehot_colum = pd.get_dummies(table[colum], prefix=colum)
+            table = pd.merge(left=table, right=onehot_colum, left_index=True, right_index=True)
+            table = table.drop(columns=colum)
+
+        # Normalize continous columns
+        for column in ['age', 'fnlwgt', 'education-num', 'capital-gain', 'capital-loss', 'hours-per-week']:
+            m = table[column].mean()
+            v = table[column].std()**2
+
+            table[column] -= m
+            table[column] /= v
         
-        self._data = data
+        self._table = table
 
     def __len__(self):
-        return len(self._data)
+        return len(self._table)
 
     def __getitem__(self, i):
         # Alias the datafram
-        df = self._data
+        df = self._table
         df_x = df.loc[:, ~df.columns.isin(['salary_ <=50K', 'salary_ >50K', 'sex_ Female' ,'sex_ Male'])]
         x = torch.Tensor(df_x.values[i])
         t = torch.Tensor([df.iloc[i]['salary_ >50K']])
@@ -67,7 +75,7 @@ class CheXpertDataset(data.Dataset):
         img = Image.open(os.path.join(self._datapath, filename))
 
         x = self.transfrom(img)
-        t = torch.Tensor([int(df.iloc[i]['No Finding'] != 1)]) # Count(1) = 22381, Count(nan) = 201033
+        t = torch.Tensor([int(df.iloc[i]['Pleural Effusion'] == 1)]) # Count(1) = 22381, Count(nan) = 201033
         d = torch.Tensor([df.iloc[i]['Support Devices']]) # Count(1) = 116001, Count(nan) = 0,  Count(0.) = 6137, Count(-1.) = 1079
         return x, t, d
 
@@ -98,6 +106,6 @@ def get_test_set(dataset:str, root="data/"):
 #     a = dummy[1]
 #     print(a)
 #     print(len(dummy))
-#     print(dummy[0]._data.head())
-#     print(a)
+#     print(dummy._table.columns)
+#     # print(a)
 #     # dummy2 = get_celeba()
