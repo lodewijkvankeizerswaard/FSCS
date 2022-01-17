f7a14ea146179b3e68a9adab71c39003c62746aa
tijmenvanetten <tijmenettenvan@gmail.com>
2022-01-17 11:16:11 +0100

    Externalize get_model

--- model.py
+++ model.py
@@ -5,52 +5,9 @@ import torch.nn.functional as F
 
 import torchvision.models as models
 
+from featurizers import get_model
+
 import numpy as np
-
-ADULT_DATASET_FEATURE_SIZE = 105
-NODE_SIZE = 80
-
-
-def drop_classification_layer(model):
-    return torch.nn.Sequential(*(list(model.children())[:-1]))
-
-
-def get_model(dataset_name: str, pretrained: bool=True):
-    """
-    Returns the model architecture for the provided dataset_name. 
-    """
-    if dataset_name == 'adult':
-        model = nn.Sequential(
-            nn.Linear(ADULT_DATASET_FEATURE_SIZE, NODE_SIZE),
-            nn.SELU()
-            )
-        out_features = NODE_SIZE
-
-    elif dataset_name == 'celeba':
-        model = models.resnet50(pretrained=pretrained)
-        model = drop_classification_layer(model)
-        out_features = 2048
-
-    elif dataset_name == 'civilcomments':
-        bert_model = torch.hub.load('huggingface/pytorch-transformers', 'model', 'bert-base-uncased')
-        fc_model = nn.Sequential(
-            nn.Linear(1024, NODE_SIZE),
-            nn.SELU()
-            )
-        model = torch.nn.Sequential(bert_model, fc_model)
-        out_features = NODE_SIZE
-
-    elif dataset_name =='chexpert':
-        model = models.densenet121(pretrained=pretrained)
-        model = drop_classification_layer(model)
-        model = nn.Sequential(model, nn.AdaptiveAvgPool2d((1,1)))
-        out_features = 1024
-        
-    else:
-        assert False, f'Unknown network architecture \"{dataset_name}\"'
-        
-    return out_features, model
-
 
 class FairClassifier(nn.Module):
     def __init__(self, input_model: str, nr_attr_values: int = 2):
