70d4a1b07cf0827eb64234921109379efe89179a
Fabian van Stijn <fabianvanstijn@gmail.com>
2022-01-17 22:32:14 +0100

    Celeba afgerond en geprobeerd te trainen

--- data.py
+++ data.py
@@ -15,6 +15,7 @@ from torchvision import transforms
 # Dataset attribute selection. Please make sure that the column name and column values are correct.
 CHEXPERT_ATTRIBUTE = {'column' : 'Pleural Effusion', 'values' : [0, 1]}
 ADULT_ATTRIBUTE = {'column' : 'sex', 'values' : [' Male', ' Female']}
+CELEBA_ATTRIBUTE = {'column' : 'Male', 'values' : [-1, 1]}
 # ADULT_ATTRIBUTE = {'column' : 'relationship', 'values' : [' Husband', ' Not-in-family', ' Wife', ' Own-child', ' Unmarried', ' Other-relative']}
 
 # Editing these global variables has a very high chance of breaking the data
@@ -242,73 +243,6 @@ def get_test_set(dataset:str, root="data/"):
         raise ValueError("This dataset is not implemented")
     return test
 
-# class CelebA(data.Dataset):
-#     def __init__(self, root, split = "train"):
-#         datapath = root + "/celeba"
-#         assert os.path.exists(datapath), "CelebA dataset not found! Did you run 'get_data.sh'?"
-#
-#         self._filename = "celeba.test" if split == "test" else "adult.data"
-#         self.url = 'https://drive.google.com/uc?id=1cNIac61PSA_LqDFYFUeyaQYekYPc75NH'
-
-
-# def get_celeba(root="data"):
-#     assert 5 == 0, "CelebA cannot be downloaded through pytorch. Please see https://github.com/pytorch/vision/issues/1920"
-#     train = CelebA(root=root, split="train", download=True)
-#     val = CelebA(root=root, split="val", download=True)
-#     test = CelebA(root=root, split="test", download=True)
-#     return (train, val, test)
-
-def get_celeba(root = "data"):
-    data_root = root + "/celeba"
-    dataset_url = 'https://drive.google.com/uc?id=1cNIac61PSA_LqDFYFUeyaQYekYPc75NH'
-    download_path = f'{data_root}/img_align_celeba.zip'
-    dataset_folder = f'{root}/img_align_celeba'
-    if os.path.exists(download_path):
-        print("CelebA dataset already downloaded!")
-        return
-    if not os.path.exists(data_root):
-        os.makedirs(data_root)
-        os.makedirs(dataset_folder)
-    gdown.download(dataset_url, download_path, quiet = False)
-    with zipfile.ZipFile(download_path, 'r') as zip:
-        zip.extractall(dataset_folder)
-
-def preprocess_celeba(img_dir = "data\celeba\img_align_celeba\img_align_celeba"):
-    image_names = os.listdir(img_dir)
-    img_path = os.path.join(img_dir, image_names[0])
-    print(img_path)
-
-
-
-# def get_celeba(root="data"):
-#     assert 5 == 0, "CelebA cannot be downloaded through pytorch. Please see https://github.com/pytorch/vision/issues/1920"
-#     train = CelebA(root=root, split="train", download=True)
-#     val = CelebA(root=root, split="val", download=True)
-#     test = CelebA(root=root, split="test", download=True)
-#     return (train, val, test)
-
-# def get_celeba(root = "data"):
-#     data_root = root + "/celeba"
-#     dataset_url = 'https://drive.google.com/uc?id=1cNIac61PSA_LqDFYFUeyaQYekYPc75NH'
-#     download_path = f'{data_root}/img_align_celeba.zip'
-#     dataset_folder = f'{root}/img_align_celeba'
-#     if os.path.exists(download_path):
-#         print("CelebA dataset already downloaded!")
-#         return
-#     if not os.path.exists(data_root):
-#         os.makedirs(data_root)
-#         os.makedirs(dataset_folder)
-#     gdown.download(dataset_url, download_path, quiet = False)
-#     with zipfile.ZipFile(download_path, 'r') as zip:
-#         zip.extractall(dataset_folder)
-
-# def preprocess_celeba(img_dir = "data\celeba\img_align_celeba\img_align_celeba"):
-#     image_names = os.listdir(img_dir)
-#     img_path = os.path.join(img_dir, image_names[0])
-#     print(img_path)
-
-# def validation_split(directory):
-#     validation_data = pd.read_csv(directory, sep=" ", header=None)
     
 
 class CelebADataset(data.Dataset):
@@ -318,11 +252,8 @@ class CelebADataset(data.Dataset):
 
         self.split_filename = "list_eval_partition.txt"
         self.anno_filename = "list_attr_celeba.txt"
-        self.split_table = pd.read_csv(os.path.join(self._datapath, self.split_filename), sep="\t", header = 1)
-        self.split_table.columns = ["image", "partition"]
-        self.anno_table = pd.read_csv(os.path.join(self._datapath, self.anno_filename), sep="\t", header = 0)
-
-        print(self.split_table.head())
+        self.split_table = pd.read_csv(os.path.join(self._datapath, self.split_filename), sep=" ", header = None, names=["image", "partition"])
+        self.anno_table = pd.read_csv(os.path.join(self._datapath, self.anno_filename), sep=r"\s+", header = 1)
 
         if split == "train":
             self.split_table = self.split_table[self.split_table.partition == 0]
@@ -331,57 +262,67 @@ class CelebADataset(data.Dataset):
         else:
             self.split_table = self.split_table[self.split_table.partition == 2]
 
+        index_list = list(self.split_table.index.values)
+        self.anno_table = self.anno_table.iloc[index_list]
+
+        probs = self._attr_ratio(self.anno_table)
+        self._attr_dist = torch.distributions.Categorical(probs=probs)
+
         self.transform = transforms.ToTensor()
-    
+
+    def _attr_ratio(self, table: pd.DataFrame) -> torch.Tensor:
+        """Finds the ratio in which the attribute occurs in the data set, such that we can later
+        sample from this distribution. 
+
+        Args:
+            table (pd.DataFrame): the table from which to obtain the attribute ratio.
+
+        Returns:
+            torch.Tensor: a tensor with probabilities for the ADULT_ATTRIBUTE['values'] in the same order.
+        """
+        counts = table[CELEBA_ATTRIBUTE['column']].value_counts()
+        ratios = torch.Tensor([counts[attr_val] for attr_val in CELEBA_ATTRIBUTE['values']])
+        return ratios / sum(ratios) 
+
+    def sample_d(self, size: tuple) -> torch.Tensor:
+        return self._attr_dist.sample(size)
+
+    def datapoint_shape(self) -> torch.Tensor:
+        """Return the amount of elements in each x value
+
+        Returns:
+            int: the amount of elements in x
+        """
+        return self[0][0].shape
+
+    def nr_attr_values(self) -> int:
+        """Returns the number of possible values for the attribute of this dataset.
+
+        Returns:
+            int: the number of attributes
+        """
+        return len(CELEBA_ATTRIBUTE['values'])
+
+    def __len__(self):
+        return len(self._table)
+
     def __getitem__(self, i):
         # Alias the dataframe and the evaluation partition
         df = self.anno_table
         df_split = self.split_table
 
+
         # Get the image from the training or test data
         filename = df_split.iloc[i]["image"]
         img = Image.open(os.path.join(self._datapath, "img_align_celeba", filename)) 
 
-        resized_image = image.resize(224, 224)
-        x = self.transfrom(img)
+
+        resized_image = img.resize((224, 224))
+
+        x = self.transform(resized_image)
         t = torch.Tensor([int(df.iloc[i]['Blond_Hair'] == 1)])
         d = torch.Tensor([int(df.iloc[i]['Male'] == 1)])
         return x, t, d
-     
-
-
-
-        
-
-    # class CheXpertDataset(data.Dataset):
-    # # TODO add docstring
-    # # TODO change target to Pleural Effusion
-    # # TODO image preprocessing
-    # # TODO improve comments
-    # def __init__(self, root, split="train"):
-    #     self._datapath = os.path.join(root, "chexpert")
-    #     assert os.path.exists(self._datapath), "CheXpert dataset not found! Did you run `get_data.sh`?"
-        
-    #     self._filename = "train.csv" if split == "train" else "valid.csv"
-    #     self._table = pd.read_csv(os.path.join(self._datapath, "CheXpert-v1.0-small", self._filename))
-
-    #     self.transfrom = transforms.ToTensor()
-
-    # def __getitem__(self, i):
-    #     # Alias the dataframe
-    #     df = self._table
-
-    #     # Get the image
-    #     filename = df.iloc[i]['Path']
-    #     img = Image.open(os.path.join(self._datapath, filename))
-
-    #     x = self.transfrom(img)[:,:224,:224].repeat(3,1,1)
-    #     t = torch.Tensor([int(df.iloc[i]['Pleural Effusion'] == 1)]) # Count(1) = 22381, Count(nan) = 201033
-    #     d = torch.Tensor([int(df.iloc[i]['Support Devices'] == 1)]) # Count(1) = 116001, Count(nan) = 0,  Count(0.) = 6137, Count(-1.) = 1079
-    #     return x, t, d
-
-    # def __len__(self):
-    #     return len(self._table)
 
 
 
@@ -393,5 +334,5 @@ def get_chexpert(root="data"):
     pass
 
 if __name__ == "__main__":
-    print("hello")
     celeba = CelebADataset(root="data")
+    print(celeba.datapoint_shape)
