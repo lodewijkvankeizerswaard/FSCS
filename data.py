d30b0babc3703626150051f6fc96d18e1c1a458c
lodewijkvkeizerswaard <lodewijk.keizer@gmail.com>
2022-01-19 14:21:05 +0100

    Fix bias Adult

--- data.py
+++ data.py
@@ -48,8 +48,8 @@ class AdultDataset(data.Dataset):
             table['native-country_ Holand-Netherlands'] = np.zeros(len(table))
         else:
             # Introduce bias in the train data
-            where_d_zero = set((table[ADULT_ATTRIBUTE['column']] == ADULT_ATTRIBUTE['values'][0]).index)
-            where_y_one = set((table['salary_ >50K'] == 1).index)
+            where_d_zero = set(table[ table[ADULT_ATTRIBUTE['column']] == ADULT_ATTRIBUTE['values'][0] ].index)
+            where_y_one = set(table[ table['salary_ >50K'] == 1 ].index)
             bias = list(where_d_zero & where_y_one)[50:]
             table = table.drop(index=bias)
         
